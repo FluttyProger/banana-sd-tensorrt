b487bd147a96d52f42608b5c4fadc43b2c4ce7f7
FluttyProger <ktaraso@yandex.ru>
2023-04-24 08:55:54 +0300

    Update download.py

--- download.py
+++ download.py
@@ -1,16 +1,30 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
-
+import os
 import torch
-from diffusers.utils import load_image
-from diffusers import UniPCMultistepScheduler, StableDiffusionControlNetPipeline, ControlNetModel
+from torch import autocast
+import base64
+from io import BytesIO
+from transformers import pipeline
+from diffusers import DDIMScheduler
+from diffusers.pipelines.stable_diffusion import StableDiffusionPipeline
 
 def download_model():
-    # do a dry run of loading the huggingface model, which will download weights
-    controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-canny", torch_dtype=torch.float16)
-    model = StableDiffusionControlNetPipeline.from_pretrained(
-        "runwayml/stable-diffusion-v1-5", controlnet=controlnet, safety_checker=None, torch_dtype=torch.float16
-    )
+    model_name = os.getenv("MODEL_NAME")
+    model_rev = os.getenv("MODEL_REV")
+    scheduler = DDIMScheduler.from_pretrained(model_name, subfolder="scheduler")
+    
+    model = StableDiffusionPipeline.from_pretrained(model_name,
+                                                    custom_pipeline="stable_diffusion_tensorrt_txt2img_mine",
+                                                    revision=model_rev,
+                                                    torch_dtype=torch.float16,
+                                                    scheduler=scheduler)
+    
+    # re-use cached folder to save ONNX models and TensorRT Engines
+    model.set_cached_folder(model_name, revision=model_rev)
+    
+    model = model.to("cuda")
+
 
 if __name__ == "__main__":
     download_model()
