0cd8e77089f2f07537dc4cbc0538e04f4483b62e
FluttyProger <ktaraso@yandex.ru>
2023-04-29 19:20:49 +0300

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -244,9 +244,9 @@ class Engine():
             assert isinstance(buf, cuda.DeviceView)
             device_buffers[name] = buf
         bindings = [0] * start_binding + [buf.ptr for buf in device_buffers.values()]
-        # noerror = self.context.execute_async_v2(bindings=bindings, stream_handle=stream.ptr)
-        # if not noerror:
-        #     raise ValueError(f"ERROR: inference failed.")
+        noerror = self.context.execute_async_v2(bindings=bindings, stream_handle=stream.ptr)
+        if not noerror:
+            raise ValueError(f"ERROR: inference failed.")
 
         return self.tensors
 
