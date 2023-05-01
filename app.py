620dba87ec1f34fa2c1a1a0479e8b42e0cb6d5be
FluttyProger <ktaraso@yandex.ru>
2023-05-01 22:43:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,42 @@
 import os
 import base64
 from io import BytesIO
-from potassium import Potassium, Request, Response
 import tensorrt as trt
+from sanic import Sanic, response
 from utilities import TRT_LOGGER, add_arguments
 from txt2img_pipeline import Txt2ImgPipeline
 
-app = Potassium("my_app")
+model = None
+
+server = Sanic("my_app")
 
 os.environ['CUDA_MODULE_LOADING'] = 'LAZY'
 
-@app.init
-def init():
-    trt.init_libnvinfer_plugins(TRT_LOGGER, '')
-    demo = Txt2ImgPipeline(
-        output_dir="",
-        version="1.5",
-        hf_token="",
-        max_batch_size=4)
-    demo.loadEngines("/deliberate-model/engine")
-    context = {
-        "model": demo
-    }
-    return context
+trt.init_libnvinfer_plugins(TRT_LOGGER, '')
+model = Txt2ImgPipeline(
+    output_dir="",
+    version="1.5",
+    hf_token="",
+    max_batch_size=4)
+model.loadEngines("/deliberate-model/engine")
 
+@server.route('/healthcheck', methods=["GET"])
+def healthcheck(request):
+    # dependency free way to check if GPU is visible
+    gpu = False
+    out = subprocess.run("nvidia-smi", shell=True)
+    if out.returncode == 0: # success state on shell command
+        gpu = True
 
-@app.handler()
-def handler(context, request: Request) -> Response:
-    model = context.get("model")
-    model_inputs = request.json
-    outputs = inference(model, model_inputs)
-    
-    return Response(json={"outputs": outputs}, status=200)
+    return response.json({"state": "healthy", "gpu": gpu})
 
-
-# Inference is ran for every server call
-# Reference your preloaded global model variable here.
-
-
-def inference(model, model_inputs: dict) -> dict:
+@server.route('/', methods=["POST"]) 
+def handler(request):
+    global model
+    try:
+        model_inputs = response.json.loads(request.json)
+    except:
+        model_inputs = request.json
     prompt = model_inputs.get('prompt', None)
     height = model_inputs.get('height', 768)
     negative = model_inputs.get('negative_prompt', None)
@@ -48,7 +46,7 @@ def inference(model, model_inputs: dict) -> dict:
     seed = model_inputs.get('seed', -1)
     sampler = model_inputs.get('sampler', "DDIM")
 
-    if not prompt: return {'message': 'No prompt was provided'}
+    if not prompt: return response.json({'message': 'No prompt was provided'})
 
 
     # Load TensorRT engines and pytorch modules
@@ -60,8 +58,9 @@ def inference(model, model_inputs: dict) -> dict:
     buffered = BytesIO()
     images[0].save(buffered, format="JPEG")
     image_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
-    return {'image_base64': image_base64}
+    
+    return response.json({"outputs": outputs})
 
 
 if __name__ == "__main__":
-    app.serve()
+    server.run(host='0.0.0.0', port=8000, workers=1)
